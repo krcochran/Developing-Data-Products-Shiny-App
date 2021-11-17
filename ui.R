2f745657d3175383596fb21bcdbdb22e26777861
Kyle Cochran <KyleCochran@Kyles-MacBook-Pro.local>
2021-11-17 18:13:53 -0500

    Shiny Project Upload

---
+++ ui.R
@@ -0,0 +1,17 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+    sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+    "then x times 5 is",
+    textOutput("product")
+))
